20edda9ba151f570f2c945120e0f9b5dbd1ffd6c
Zoltan Fulop <zfulop@zolilla.com>
2014-06-19 21:52:34 +0200

    .

--- recepcio/includes/config.php
+++ recepcio/includes/config.php
@@ -5,7 +5,7 @@ define('DB_USER', 'maverick');
 define('DB_PASSWORD', 'maverick');
 define('DB_NAME', 'maverickhos_3637');
 
-define('PHP_MAILER_LANGUAGE_DIR', "c:/Projects/maverick/recepcio/includes/phpmailer/language");
+define('PHP_MAILER_LANGUAGE_DIR', "c:/Projects/maverick/Repository/recepcio/includes/phpmailer/language");
 define('PHP_MAILER_SENDTYPE_SENDMAIL', 'SENDMAIL');
 define('PHP_MAILER_SENDTYPE_MAIL', 'MAIL');
 define('PHP_MAILER_SENDTYPE_SMTP', 'SMTP');
@@ -18,10 +18,10 @@ define('PHP_MAILER_SENDTYPE_SMTP_SECURE', '');
 define('PHP_MAILER_SENDTYPE_SMTP_USER', 'zoltanfulop74@t-online.hu');
 define('PHP_MAILER_SENDTYPE_SMTP_PASSWORD', 'gsm4nmH7');
 
-define('EMAIL_IMG_DIR', "c:/Projects/maverick/www/img/email/");
+define('EMAIL_IMG_DIR', "c:/Projects/maverick/Repository/www/img/email/");
 
 define('IMG_ROOT_URL', 'http://localhost-maverick/img/');
-define('IMG_ROOT_DIR', 'c:/Projects/maverick/www/img/');
+define('IMG_ROOT_DIR', 'c:/Projects/maverick/Repository/www/img/');
 
 define('ROOMS_IMG_DIR', IMG_ROOT_DIR . "rooms/");
 define('ROOMS_IMG_URL', IMG_ROOT_URL . "rooms/");
@@ -34,11 +34,11 @@ define('ROOT_URL', '/');
 define('LOCATION', 'lodge');
 define('CONTACT_EMAIL', 'reservation@mavericklodges.com');
 
-define('EXCHANGE_TABLE_FILE', 'c:/Projects/maverick/recepcio/includes/exchange_table.php');
+define('EXCHANGE_TABLE_FILE', 'c:/Projects/maverick/Repository/recepcio/includes/exchange_table.php');
 
-define('FPDF_FONTPATH','c:/Projects/maverick/recepcio/includes/font');
+define('FPDF_FONTPATH','c:/Projects/maverick/Repository/recepcio/includes/font');
 
 define('CONFIRM_BOOKING_URL', 'http://localhost-maverick/LANG/confirm_booking.php?CONFIRM_CODE');
-define('LANG_DIR', 'c:/Projects/maverick/www/includes/language/');
+define('LANG_DIR', 'c:/Projects/maverick/Repository/www/includes/language/');
 
 ?>
