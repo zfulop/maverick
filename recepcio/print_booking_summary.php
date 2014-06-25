5aa16cbc0a69d84c495a690bee0473447e6ea55d
Zoltan Fulop <zfulop@zolilla.com>
2014-06-25 20:21:24 +0200

    .

--- recepcio/print_booking_summary.php
+++ recepcio/print_booking_summary.php
@@ -7,21 +7,10 @@ $link = db_connect();
 
 $descrId = intval($_REQUEST['description_id']);
 
-$rooms = array();
-$sql = "SELECT rooms.*, lang_text.value AS room_name FROM rooms INNER JOIN lang_text ON (lang_text.row_id=rooms.room_type_id AND lang_text.table_name='room_types' AND lang_text.column_name='name' AND lang_text.lang='eng')";
-$result = mysql_query($sql, $link);
-if(!$result) {
-	set_error("Cannot get rooms.");
-	trigger_error("Cannot get rooms in admin interface: " . mysql_error($link) . " (SQL: $sql)", E_USER_ERROR);
-	mysql_close($link);
-	header('Location: ' . $_SERVER['HTTP_REFERER']);
-	return;
-}
-$roomsHtmlOptions = '';
-while($row = mysql_fetch_assoc($result)) {
-	$rooms[$row['id']] = $row;
-	$roomsHtmlOptions .= '<option value="' . $row['id'] . '">' . $row['room_name'] . '</option>';
-}
+
+require(LANG_DIR . 'eng.php');
+
+$locationName = constant('LOCATION_NAME_' . strtoupper(LOCATION));
 
 $bookingDescription = null;
 $sql = "SELECT * FROM booking_descriptions WHERE id=$descrId";
@@ -38,7 +27,7 @@ $bookingDescription = mysql_fetch_assoc($result);
 
 $bookings = array();
 $type = '';
-$sql = "SELECT * FROM bookings WHERE description_id=$descrId";
+$sql = "SELECT b.*, lt.value as room_type_name FROM bookings b INNER JOIN rooms r ON b.room_id=r.id INNER JOIN lang_text lt ON (lt.table_name='room_types' AND lt.column_name='name' AND lt.lang='eng' AND lt.row_id=r.room_type_id) WHERE b.description_id=$descrId";
 $result = mysql_query($sql, $link);
 if(!$result) {
 	set_error("Cannot get booking (with description_id: $descrId).");
@@ -55,7 +44,6 @@ if(mysql_num_rows($result) < 1) {
 }
 
 while($row = mysql_fetch_assoc($result)) {
-	$row['room_name'] = $rooms[$row['room_id']]['room_name'];
 	$bookings[] = $row;
 }
 
@@ -83,22 +71,11 @@ $fnight = str_replace('/', '-', $bookingDescription['first_night']);
 $lnight = str_replace('/', '-', $bookingDescription['last_night']);
 $numOfNights = $bookingDescription['num_of_nights'];
 if($numOfNights < 1) {
-	$numOfNights = intval((strtotime(str_replace('/', '-', $lnight)) - strtotime(str_replace('/', '-', $fnight))) / (60*60*24)) + 1;
-}
-
-$sql = "SELECT * FROM booking_guest_data WHERE booking_description_id=$descrId";
-$result = mysql_query($sql, $link);
-if(!$result) {
-	$err = "Cannot get guest data of booking (with description_id: $descrId).";
-	set_error($err);
-	trigger_error($err . " SQL Error: " . mysql_error($link) . " (SQL: $sql)", E_USER_ERROR);
-	mysql_close($link);
-	header('Location: ' . $_SERVER['HTTP_REFERER']);
-	return;
+	$numOfNights = round((strtotime(str_replace('/', '-', $lnight)) - strtotime(str_replace('/', '-', $fnight))) / (60*60*24)) + 1;
 }
 
 $payments = array();
-$sql = "SELECT * FROM payments WHERE booking_description_id=$descrId";
+$sql = "SELECT * FROM payments WHERE booking_description_id=$descrId AND storno<>1";
 $result = mysql_query($sql, $link);
 if(!$result) {
 	$err = "Cannot get payment(s) of booking (with description_id: $descrId).";
@@ -134,8 +111,13 @@ $pdf = new FPDF();
 $pdf->AddPage();
 
 $pdf->SetFont('Helvetica','B',16);
-$pdf->SetX(70);
-$pdf->Cell(100, 7, "Booking Summary", 0);
+$pdf->SetX(30);
+$pdf->Ln();
+$pdf->Ln();
+$pdf->Cell(200, 7, "$locationName - Payment Receipt", 0);
+$pdf->Image('maverick_' . LOCATION . '.jpg', 145, 5, 40, 20);
+$pdf->Ln();
+$pdf->Ln();
 $pdf->Ln();
 $pdf->Ln();
 
@@ -167,8 +149,8 @@ $pdf->Cell(40, 7, "Rooms/Beds:", 0);
 $pdf->Ln();
 $pdf->SetFont('Helvetica','B',10);
 $pdf->Cell(70, 7, "Room name");
-$pdf->Cell(30, 7, "Type");
-$pdf->Cell(50, 7, "# of beds");
+$pdf->Cell(25, 7, "Type");
+$pdf->Cell(40, 7, "# of beds");
 $pdf->Cell(50, 7, "Price");
 $pdf->Ln();
 
@@ -176,18 +158,18 @@ $pdf->SetFont('Helvetica','',10);
 $roomTotal = 0;
 foreach($bookings as $booking) {
 	$bid = $booking['id'];
-	$pdf->Cell(70, 7, $booking['room_name']);
-	$pdf->Cell(30, 7, $booking['booking_type']);
-	$pdf->Cell(50, 7, $booking['num_of_person']);
-	$pdf->Cell(50, 7, floatval($booking['room_payment']) . " euro");
+	$pdf->Cell(70, 7, $booking['room_type_name']);
+	$pdf->Cell(25, 7, $booking['booking_type']);
+	$pdf->Cell(40, 7, $booking['num_of_person']);
+	$pdf->Cell(50, 7, floatval($booking['room_payment']) . " EUR");
 	$pdf->Ln();
 	$roomTotal += $booking['room_payment'];
 }
 
-$pdf->Cell(100, 7, "");
+$pdf->Cell(95, 7, "");
 $pdf->SetFont('Helvetica','B',10);
-$pdf->Cell(50, 7, "Total room price");
-$pdf->Cell(50, 7, $roomTotal . " euro");
+$pdf->Cell(40, 7, "Total room price");
+$pdf->Cell(50, 7, $roomTotal . " EUR");
 
 $pdf->Ln();
 $pdf->Ln();
@@ -195,36 +177,46 @@ $pdf->Ln();
 
 $serviceChargeTotal = 0;
 if(count($serviceCharges) > 0) {
+	/*
 	$pdf->SetFont('Helvetica','B',14);
 	$pdf->Cell(40, 7, "Service charges", 0);
 	$pdf->Ln();
 	$pdf->SetFont('Helvetica','B',10);
-	$pdf->Cell(50, 7, "Name");
 	$pdf->Cell(50, 7, "Date");
+	$pdf->Cell(85, 7, "Comment");
 	$pdf->Cell(50, 7, "Price");
-	$pdf->Cell(50, 7, "Comment");
 	$pdf->Ln();
 
 	$pdf->SetFont('Helvetica','',10);
+	 */
 	foreach($serviceCharges as $sc) {
-		$pdf->Cell(50, 7, $sc['type']);
+		$prc = sprintf('%.2f', $sc['amount']) . ' ' . $sc['currency'];
+		if($sc['currency'] != 'EUR') {
+			$prc .= ' (' . sprintf('%.2f', convertAmount($sc['amount'], $sc['currency'], 'EUR', $dt)) . ' EUR)';
+		}
+		/*
 		$pdf->Cell(50, 7, $sc['time_of_service']);
-		$pdf->Cell(50, 7, sprintf('%.2f', $sc['amount']) . ' ' . $sc['currency']);
-		$pdf->Cell(50, 7, $sc['comment']);
+		$pdf->Cell(85, 7, $sc['type'] . ' ' . $sc['comment']);
+		$pdf->Cell(50, 7, $prc);
 		$pdf->Ln();
+		*/
 
 		$dt= min(date('Y-m-d'), $lnight);
 		$serviceChargeTotal += convertAmount($sc['amount'], $sc['currency'], 'EUR', $dt);
 	}
+	/*
 	$pdf->SetFont('Helvetica','B',10);
 	$pdf->Cell(50, 7, '');
-	$pdf->Cell(50, 7, 'Service total');
-	$pdf->Cell(50, 7, sprintf("%.2f", $serviceChargeTotal) . ' euro');
+	$pdf->Cell(85, 7, 'Total charges');
+	$pdf->Cell(50, 7, sprintf("%.2f", $serviceChargeTotal) . ' EUR');
+	*/
 
 	$serviceChargeTotal = sprintf("%.2f", $serviceChargeTotal);
 
+	/*
 	$pdf->Ln();
 	$pdf->Ln();
+	*/
 }
 
 $paymentTotal = 0;
@@ -234,30 +226,34 @@ if(count($payments) > 0) {
 	$pdf->Ln();
 	$pdf->SetFont('Helvetica','B',10);
 	$pdf->Cell(50, 7, "Date");
-	$pdf->Cell(50, 7, "Comment");
-	$pdf->Cell(50, 7, "Amount");
+	$pdf->Cell(85, 7, "Comment");
+	$pdf->Cell(50, 7, "Price");
 	$pdf->Ln();
 
 	$pdf->SetFont('Helvetica','',10);
 	foreach($payments as $payment) {
-		$amount = sprintf('%.2f', $payment['amount'])	. ' ' . $payment['currency'];
-		$pdf->Cell(50, 7, $payment['time_of_payment']);
-		$pdf->Cell(50, 7, $payment['comment']);
-		$pdf->Cell(50, 7, $amount);
+		$dt = $payment['time_of_payment'];
+		$pdf->Cell(50, 7, $dt);
+		$pdf->Cell(85, 7, $payment['comment']);
+		$prc = sprintf('%.2f', $payment['amount']) . ' ' . $payment['currency'];
+		if($payment['currency'] != 'EUR') {
+			$prc .= ' (' . sprintf('%.2f', convertAmount($payment['amount'], $payment['currency'], 'EUR', $dt)) . ' EUR)';
+		}
+		$pdf->Cell(50, 7, $prc);
 		$pdf->Ln();
-		$paymentTotal += convertAmount($payment['amount'], $payment['currency'], 'EUR', $payment['time_of_payment']);
+		$paymentTotal += convertAmount($payment['amount'], $payment['currency'], 'EUR', $dt);
 	}
 
 	$pdf->SetFont('Helvetica','B',10);
 	$pdf->Cell(50, 7, '');
-	$pdf->Cell(50, 7, 'Total');
-	$pdf->Cell(50, 7, sprintf("%.2f", $paymentTotal));
+	$pdf->Cell(85, 7, 'Total payment');
+	$pdf->Cell(50, 7, sprintf("%.2f", $paymentTotal) . ' EUR');
 	$paymentTotal = sprintf("%.2f", $paymentTotal);
 }
 
 $balance = $roomTotal + $serviceChargeTotal - $paymentTotal;
 $balanceHuf = convertAmount($balance, 'EUR', 'HUF', date('Y-m-d'));
-$balance = sprintf("%.2f", $balance);
+$balance = intval($balance);
 $balanceHuf = floor($balanceHuf/100) * 100;
 
 $pdf->Ln();
@@ -265,11 +261,22 @@ $pdf->Ln();
 
 
 $pdf->SetFont('Helvetica','B',14);
-$pdf->Cell(100, 7, "Balance: $balance euro ($balanceHuf Ft)", 0);
+$pdf->Cell(30, 7, "Balance:");
+$pdf->Cell(60, 7, "$balance EUR ($balanceHuf Ft)", 0);
+$pdf->Ln();
+$pdf->Cell(30, 7, "Date: ");
+$pdf->Cell(60, 7, date('Y-m-d'), 0);
 
 
-header('Content-type: applcation/pdf');
-$pdf->Output('booking_summary_' . $descrId . '.pdf', 'I');
+if(isset($_REQUEST['action']) and ($_REQUEST['action'] == 'email')) {
+	$pdf->Output('payment_receipt_' . $descrId . '.pdf', 'F');
+	$result = sendMail(CONTACT_EMAIL, $locationName, $email, $name, $locationName . ' - Payment Receipt', "Dear $name,\n\nThank you for visiting $locationName. Please find the payment receipt attached.", array(), array('payment_receipt_' . $descrId . '.pdf'));
+	set_message("email was sent to $email. View receipt: <a href=\"payment_receipt_$descrId" . ".pdf\">PDF</a>");
+	header("Location: " . $_SERVER['HTTP_REFERER']);
+} else {
+	header('Content-type: applcation/pdf');
+$pdf->Output('payment_receipt_' . $descrId . '.pdf', 'I');
+}
 
 
 
